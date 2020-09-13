067638344055f2750b171e2f793821a4d8ef207b
Rafael Bardini <rafael@rbardini.com>
2020-09-13 02:12:31 +0200

    Replace deprecated addParameters and addDecorator APIs

--- example/.storybook/preview.js
+++ example/.storybook/preview.js
@@ -1,13 +1,14 @@
-import { addDecorator, addParameters } from '@storybook/react';
 import { withPlayroom } from 'storybook-addon-playroom';
 
-addDecorator(withPlayroom); // before any other decorators
+export const decorators = [
+  withPlayroom, // before any other decorators
+];
 
-addParameters({
+export const parameters = {
   playroom: {
     // Because Playroom is built inside Storybook on this example's deploy,
     // we must define the absolute path to it when NODE_ENV is production,
     // otherwise set undefined to use the default Playroom URL (localhost)
     url: process.env.NODE_ENV === 'production' ? '/playroom/' : undefined,
   },
-});
+};
