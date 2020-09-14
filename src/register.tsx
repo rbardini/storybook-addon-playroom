882f1d0cc5e7a4c79f623a793012927dfe7bd1a7
Rafael Bardini <rafael@rbardini.com>
2020-09-14 02:41:20 +0200

    Use panel ID for tab

--- src/register.tsx
+++ src/register.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { addons, types } from '@storybook/addons';
 
-import { ADDON_ID, PARAM_KEY } from './constants';
+import { ADDON_ID, PANEL_ID, PARAM_KEY } from './constants';
 import Panel from './Panel';
 
 addons.register(ADDON_ID, () => {
   const channel = addons.getChannel();
 
-  addons.add(ADDON_ID, {
+  addons.add(PANEL_ID, {
     title: 'Playroom',
     type: types.TAB,
     route: ({ storyId }) => `/playroom/${storyId}`,
