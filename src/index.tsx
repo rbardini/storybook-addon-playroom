adcf89a973960001a36f47b6a4939bf899686de2
Rafael Bardini <rafael@rbardini.com>
2020-09-15 11:59:55 +0200

    Format all files

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,11 @@ import React, { FC } from 'react';
 import reactElementToJSXString from 'react-element-to-jsx-string';
 import base64Url from 'base64-url';
 import {
-  addons, makeDecorator, WrapperSettings, StoryGetter, StoryContext,
+  addons,
+  makeDecorator,
+  WrapperSettings,
+  StoryGetter,
+  StoryContext,
 } from '@storybook/addons';
 
 import { EVENTS, PARAM_KEY } from './constants';
@@ -13,20 +17,13 @@ type Props = {
   getStory: StoryGetter;
   context: StoryContext;
   settings: WrapperSettings;
-}
+};
 
-const Story: FC<Props> = ({
-  getStory,
-  context,
-  settings: {
-    parameters,
-  },
-}) => {
-  const {
-    url, code, reactElementToJSXStringOptions,
-  } = getOptions(parameters);
+const Story: FC<Props> = ({ getStory, context, settings: { parameters } }) => {
+  const { url, code, reactElementToJSXStringOptions } = getOptions(parameters);
   const story = getStory(context);
-  const jsxString = code || reactElementToJSXString(story, reactElementToJSXStringOptions);
+  const jsxString =
+    code || reactElementToJSXString(story, reactElementToJSXStringOptions);
   const channel = addons.getChannel();
   const codeUrl = url && `${url}#?code=${base64Url.encode(jsxString)}`;
 
@@ -39,10 +36,6 @@ export const withPlayroom = makeDecorator({
   name: 'withPlayroom',
   parameterName: PARAM_KEY,
   wrapper: (getStory, context, settings) => (
-    <Story
-      getStory={getStory}
-      context={context}
-      settings={settings}
-    />
+    <Story getStory={getStory} context={context} settings={settings} />
   ),
 });
